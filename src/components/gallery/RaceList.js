045aec8f4a673cd0edd77ebbf01fae11d9c3b03d
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2020-07-05 15:05:20 -0500

    Gallery view base

---
+++ src/components/gallery/RaceList.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const RaceList = params => {
+  return (
+    <div>
+      <h2>Razas</h2>
+      <ul className="list-group">
+        <a
+          href="#"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+        >
+          Persa
+          <span className="badge badge-primary badge-pill">14</span>
+        </a>
+        <a
+          href="#"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+        >
+          Bengala
+          <span className="badge badge-primary badge-pill">2</span>
+        </a>
+        <a
+          href="#"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+        >
+          Siamés
+          <span className="badge badge-primary badge-pill">1</span>
+        </a>
+        <a
+          href="#"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center"
+        >
+          Coon
+          <span className="badge badge-primary badge-pill">1</span>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default RaceList;
