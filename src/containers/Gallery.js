3cb02730262e9f26941a4f69a8bef6fb208c33a8
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2020-07-06 11:25:34 -0500

    Diseño de Menu de opciones añadido

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
 import ImageCard from "../components/gallery/ImageCard";
-import RaceList from '../components/gallery/RaceList'
+import RaceList from "../components/gallery/RaceList";
+import MenuOptions from "../components/gallery/MenuOptions";
 
 export default class Gallery extends Component {
   render() {
     return (
       <div className="container p-4 d-flex col-12">
-
         <div className="col-3">
-          <RaceList/>
+          <RaceList />
         </div>
 
-        <div className="col-5">
+        <div className="col-6">
           <ImageCard
             title="Gatita Mimosa"
             description="Puta madre pablo"
@@ -20,6 +20,10 @@ export default class Gallery extends Component {
             like="50"
             dislike="1"
           />
+        </div>
+        <div className="col-3">
+          <MenuOptions header="Top gatos amados"/>
+          <MenuOptions header="Top gatos no tan amados"/>
         </div>
       </div>
     );
