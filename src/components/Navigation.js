a9ebd9657009a416493768e4d4428b88a9805bea
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2020-07-06 11:45:17 -0500

    Cambios en la navegacion minimos

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary pl-5">
       <Link className="navbar-brand" to="/">
         Cats
       </Link>
